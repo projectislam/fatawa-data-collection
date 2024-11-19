13d5c7f62226e8d0195914f60d97551d5cd2f512
AxeemHaider <axeemhaider@gmail.com>
2024-11-19 09:38:35 +0500

    topic 10 complete

--- darulifta-deoband.com/fetch_data.py
+++ darulifta-deoband.com/fetch_data.py
@@ -24,7 +24,7 @@ def get_start_page(link):
     # if link == "https://darulifta-deoband.com/home/qa_ur/islamic-beliefs/1":
     #     return 78
     
-    return 56 + 1
+    return 72 + 1
 
 def get_total_pages(link):
     print("Scrapping Link", link)
@@ -140,7 +140,7 @@ time.sleep(15)
 
 print("Scrap Starting....")
 
-sequence_number = 308
+sequence_number = 324
 topic_number = 10
 
 for topic in topics[topic_number - 1:topic_number]:
