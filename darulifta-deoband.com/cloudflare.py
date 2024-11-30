8c4ae51826e36d73c28bd5d75dfb10dde7cf1442
AxeemHaider <axeemhaider@gmail.com>
2024-11-30 14:28:38 +0500

    remove data

--- darulifta-deoband.com/cloudflare.py
+++
@@ -1,9 +0,0 @@
-import cloudscraper
-
-scraper = cloudscraper.create_scraper()  # Initialize the scraper
-response = scraper.get("https://darulifta-deoband.com/home/qa_ur/islamic-beliefs/1")
-
-if response.status_code == 200:
-    print(response.text)  # Page content if bypass was successful
-else:
-    print("Failed to bypass Cloudflare.")
