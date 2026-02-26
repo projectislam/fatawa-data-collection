df39c0cf0010dce004fab72c531c34bc6d21fdcc
AxeemHaider <axeemhaider@gmail.com>
2026-02-26 12:01:06 +0500

    2 complete

---
+++ 2-almuftionline.com/README.md
@@ -0,0 +1,6 @@
+Some of results does not have fatawa number instead they have text in the `fatwa_number` field for example this one
+
+link: https://almuftionline.com/2025/01/15/hajj-ky-baiz-zaruri-masail/
+filename: 226.csv
+
+when adding this into database make sure it's a number otherwise ignore it.
--- 2-almuftionline.com/fetch_data.py
+++ 2-almuftionline.com/fetch_data.py
@@ -63,9 +63,11 @@ def get_question_detail(question):
 
     html_container = soup.select_one("div.pagelayer-content div.pagelayer-row-holder > div.pagelayer-col > div.pagelayer-col-holder")
 
+    fatwa_number_ele = soup.select_one("div.entry-content.pagelayer-post-excerpt figure.wp-block-table > table tr:nth-child(1) > td:nth-child(1)")
     category_lvl_1_ele = soup.select_one("div.entry-content.pagelayer-post-excerpt figure.wp-block-table > table tr:nth-child(1) > td:nth-child(2)")
     category_lvl_2_ele = soup.select_one("div.entry-content.pagelayer-post-excerpt figure.wp-block-table > table tr:nth-child(1) > td:nth-child(3)")
 
+    fatwa_number = ""
     category_lvl_1 = ""
     category_lvl_2 = ""
 
@@ -78,6 +80,9 @@ def get_question_detail(question):
 
     if category_lvl_2_ele:
         category_lvl_2 = category_lvl_2_ele.get_text().strip()
+
+    if fatwa_number_ele:
+        fatwa_number = fatwa_number_ele.get_text().strip()
 
     content_ele = soup.select_one("div.entry-content.pagelayer-post-excerpt > div > .has-text-align-center")
 
@@ -108,7 +113,17 @@ def get_question_detail(question):
     if not answer_html:
         raise ValueError("Answer content not found")
 
+    content_ele = soup.select_one("div.entry-content.pagelayer-post-excerpt > h6.has-text-align-right")
+
+    if content_ele:
+        answer_html += str(content_ele)
+        paras = content_ele.find_next_siblings()
+
+        for para in paras:
+            answer_html += str(para)
+
     return {
+        "fatwa_number": fatwa_number,
         "category_lvl_1": category_lvl_1,
         "category_lvl_2": category_lvl_2,
         "question_html": question_html,
@@ -116,11 +131,10 @@ def get_question_detail(question):
         "html_container": str(html_container)
     }
 
+# Patches: 455-755, 755-1055, 1055-1255, 1255-1484
 
-
-
-total_pages = 1119
-start_page = 112
+total_pages = 1055
+start_page = 1024
 
 for page_number in range(start_page, total_pages + 1):
     page_link = f"{base_url}/page/{page_number}"
@@ -147,7 +161,7 @@ for page_number in range(start_page, total_pages + 1):
             "title": question["title"],
             "question_html": content["question_html"],
             "answer_html": content["answer_html"],
-            "fatwa_number": question["fatwa_number"],
+            "fatwa_number":  content["fatwa_number"] or question["fatwa_number"],
             "dar_ul_ifta": "jameya-tur-rasheed",
             "category_lvl_1": content["category_lvl_1"],
             "category_lvl_2": content["category_lvl_2"],
