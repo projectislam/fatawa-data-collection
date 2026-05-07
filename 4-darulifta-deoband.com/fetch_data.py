650a0410f3f9cf592634d8f648f2a493deecb93d
azeem-zyreon <azeem@zyreon.tech>
2026-05-07 18:14:58 +0500

    working 4 topic 25 complete

--- 4-darulifta-deoband.com/fetch_data.py
+++ 4-darulifta-deoband.com/fetch_data.py
@@ -143,8 +143,8 @@ for topic_number, topic in enumerate(topics, 1):
     category_lvl_2 = topic["category_lvl_2_text"]
     topic_link = topic["category_lvl_2_link"]
 
-    start_topic = 19
-    end_topic = 19
+    start_topic = 24
+    end_topic = 24
 
     print("Topic number:", topic_number)
 
@@ -161,8 +161,8 @@ for topic_number, topic in enumerate(topics, 1):
     print("Fetching topic....", topic_link)
 
     total_pages = get_topic_total_pages(topic_link)
-    start_page = 74
-    end_page = 141
+    start_page = 1
+    end_page = 39
 
     print(total_pages, "total pages found for topic", topic_link)
 
