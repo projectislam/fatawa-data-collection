91415e73dfa25a22a425e27e7a28ddf7193c9735
AxeemHaider <axeemhaider@gmail.com>
2026-05-08 17:43:50 +0500

    remove data for 5

--- 5-onlinefatawa.com/fetch_data.py
+++ 5-onlinefatawa.com/fetch_data.py
@@ -112,10 +112,10 @@ data_dir = "./data"
 
 os.makedirs(data_dir, exist_ok=True)
 
-total_fatawa = 13950
+total_fatawa = 18600
 fatawa_per_page = 150
 total_pages = int(total_fatawa / 150)
-start_page = 93 # start from 0
+start_page = 0 # start from 0
 
 for page_number in range(start_page, total_pages + 1):
     print("Page number", page_number)
